adc87c2585e5abbe6c221b846097345417fa6690
LunaMartin00 <164079527+LunaMartin00@users.noreply.github.com>
2025-07-04 12:54:40 -0600

    Agregando Server

---
+++ ParcialFinalPOO/src/main/java/com/mycompany/parcialfinalpoo/ParcialFinalPOO.java
@@ -0,0 +1,25 @@
+package com.mycompany.parcialfinalpoo;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+
+public class ParcialFinalPOO {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Server esperando conexiones...");
+            
+            while(true){
+                Socket socketCliente = serverSocket.accept();
+                System.out.println("CLiente conectado desde: " + socketCliente.getInetAddress());
+                new Thread(new ManejorClientes(socketCliente)).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Error en el servidor: " + e.getMessage());
+            }
+        }
+        
+}
